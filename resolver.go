698d87789dc4a8f60af4a9f02f38b6fd187acd36
Cam Hutchison <camh@xdna.net>
2020-06-27 18:14:41 +1000

    Make "existingfile" work with non-existent default
    Implement the `default:` and `env:` tags as resolvers, and change the
    `--help` output from the `BeforeApply()` hook to `BeforeResolve()`. This
    allows `--help` to work in the presence of a non-existent file as a
    default for "existingfile" (and "existingdir"). Previously the defaults
    were set very early with `Reset()`, which triggered the existingfile
    error for a non-existent file. By moving it to a resolver, they are set
    later. By moving the `--help` output from the `BeforeApply()` hook to
    the `BeforeResolve()` hook, it becomes possible to see the help message
    and what the defaults are before the non-existent file triggers the
    error.

--- resolver.go
+++ resolver.go
@@ -3,6 +3,7 @@ package kong
 import (
 	"encoding/json"
 	"io"
+	"os"
 	"strings"
 )
 
@@ -26,6 +27,34 @@ func (r ResolverFunc) Resolve(context *Context, parent *Path, flag *Flag) (inter
 	return r(context, parent, flag)
 }
 func (r ResolverFunc) Validate(app *Application) error { return nil } //  nolint: golint
+
+// DefaultsResolver resolves values from the `default` tag on a flag.
+//
+// It is installed by default. Use ClearResolvers() to disable this.
+func DefaultsResolver() Resolver {
+	return ResolverFunc(func(context *Context, parent *Path, flag *Flag) (interface{}, error) {
+		if flag.Tag.Default == "" {
+			return nil, nil
+		}
+		return flag.Tag.Default, nil
+	})
+}
+
+// EnvarResolver resolves values from environment variables.
+//
+// It is installed by default. Use ClearResolvers() to disable this.
+func EnvarResolver() Resolver {
+	return ResolverFunc(func(context *Context, parent *Path, flag *Flag) (interface{}, error) {
+		if flag.Tag.Env == "" {
+			return nil, nil
+		}
+		envar := os.Getenv(flag.Tag.Env)
+		if envar != "" {
+			return envar, nil
+		}
+		return nil, nil
+	})
+}
 
 // JSON returns a Resolver that retrieves values from a JSON source.
 //
